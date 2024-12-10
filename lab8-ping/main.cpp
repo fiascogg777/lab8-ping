f4e6806a5834f88121a63e9556f2f8e4a5a4eeb8
Ivan Boborov <ivanchik.net@mail.ru>
2024-12-10 22:17:40 +0300

    Фильтрация данных вынесена в filter.h/cpp

--- lab8-ping/main.cpp
+++ lab8-ping/main.cpp
@@ -2,32 +2,7 @@
 #include <vector>
 #include "file_reader.h"
 #include "sort.h"
-
-// Функция для фильтрации докладов по автору
-void filterByAuthor(const std::vector<Report>& reports, const std::string& surname,
-    const std::string& name, const std::string& patronymic) {
-    std::cout << "Доклады автора " << surname << " " << name << " " << patronymic << ":\n";
-    for (const auto& report : reports) {
-        if (report.surname == surname && report.name == name && report.patronymic == patronymic) {
-            std::cout << report.start << " " << report.end << " "
-                << report.surname << " " << report.name << " " << report.patronymic
-                << " " << report.topic << "\n";
-        }
-    }
-}
-
-// Функция для фильтрации докладов по длительности
-void filterByDuration(const std::vector<Report>& reports, int minDuration) {
-    std::cout << "Доклады с длительностью больше " << minDuration << " минут:\n";
-    for (const auto& report : reports) {
-        int duration = report.end - report.start;
-        if (duration > minDuration) {
-            std::cout << report.start << " " << report.end << " "
-                << report.surname << " " << report.name << " " << report.patronymic
-                << " " << report.topic << "\n";
-        }
-    }
-}
+#include "filter.h"
 
 void printReports(const std::vector<Report>& reports) {
     for (const auto& report : reports) {
